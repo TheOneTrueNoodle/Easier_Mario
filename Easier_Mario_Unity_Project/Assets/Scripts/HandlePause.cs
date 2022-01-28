59d78de2e3b6bbdf6fc58b4d76a9f7a97c754c10
ctrlcookie <regardingquiet@gmail.com>
2022-01-28 23:57:22 +0000

    Pausing now works

--- Easier_Mario_Unity_Project/Assets/Scripts/HandlePause.cs
+++ Easier_Mario_Unity_Project/Assets/Scripts/HandlePause.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 
 public class HandlePause : MonoBehaviour
 {
-    public bool paused;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool paused; //do we be paused rn
+    public GameObject pauseScreen; //the pause screen canvas object
 
     // Update is called once per frame
     void Update()
     {
         if (paused)
         {
+            pauseScreen.SetActive(true);
             Time.timeScale = 0;
         }
         else
         {
             Time.timeScale = 1;
+            pauseScreen.SetActive(false);
         }
     }
+
+    public void Pause()  //for the Resume button
+    {
+        paused = true;
+    }
+
+    public void Resume()  //for the Resume button
+    {
+        paused = false;
+    }
+
 }
