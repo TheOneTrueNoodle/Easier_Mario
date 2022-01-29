6245132a393d38028872987e7c1d39ca9ecde4e4
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2022-01-29 15:21:57 +0000

    Maybe have input changer????

---
+++ Easier_Mario_Unity_Project/Assets/InputDeviceChangeHandler.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
+
+// add this into built-in button you can create from menu: UI/Button
+public class InputDeviceChangeHandler : MonoBehaviour
+{
+    // refs to Button's components
+    public Image buttonImage;
+
+    // refs to your sprites
+    public Sprite ProControllerImage;
+    public Sprite PS4ControllerImage;
+    public Sprite keyboardImage;
+
+    void Awake()
+    {
+        buttonImage = GetComponent<Image>();
+        PlayerInput input = FindObjectOfType<PlayerInput>();
+        updateButtonImage(input.currentControlScheme);
+    }
+
+    void OnEnable()
+    {
+        InputUser.onChange += onInputDeviceChange;
+    }
+
+    void OnDisable()
+    {
+        InputUser.onChange -= onInputDeviceChange;
+    }
+
+    void onInputDeviceChange(InputUser user, InputUserChange change, InputDevice device)
+    {
+        if (change == InputUserChange.ControlSchemeChanged)
+        {
+            updateButtonImage(user.controlScheme.Value.name);
+        }
+    }
+
+    void updateButtonImage(string schemeName)
+    {
+        // assuming you have only 2 schemes: keyboard and gamepad
+        if (schemeName.Equals("Pro Controller"))
+        {
+            buttonImage.sprite = ProControllerImage;
+        }
+        else if (schemeName.Equals("M+K"))
+        {
+            buttonImage.sprite = keyboardImage;
+        }
+        else if(schemeName.Equals("PS4 Controller"))
+        {
+            buttonImage.sprite = PS4ControllerImage;
+        }
+    }
+}
