9a6694a48b23de6b1ba47590fb09a2029427e44a
AoifeBerry <71941986+AoifeBerry@users.noreply.github.com>
2022-01-29 16:49:05 +0000

    Tutorial fully playable brings you back to the menu

---
+++ Easier_Mario_Unity_Project/Assets/Scripts/Bee.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bee : MonoBehaviour
+{
+
+    public bool HaveBee = false;
+    public GameObject heart;
+    public GameObject BeeToy;
+    public GameObject PleaseHelp;
+    public GameObject thankYouText;
+    public GameObject thankYouText2;
+    private IEnumerator coroutine;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HaveBee == true)
+        {
+         
+            PleaseHelp.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bee")
+        {
+            HaveBee = true;
+            Destroy(other.gameObject);
+        }
+
+        if (other.tag == "Child")
+        {
+            heart.SetActive(true);
+            thankYouText.SetActive(true);
+            BeeToy.SetActive(true);
+            StartCoroutine(coroutineB());
+
+
+        }
+
+        IEnumerator coroutineA()
+        {
+            yield return new WaitForSeconds(3.0f);
+            SceneManager.LoadScene("Jaga Test Scene");
+        }
+        IEnumerator coroutineB()
+        {
+            yield return new WaitForSeconds(3.0f);
+            thankYouText.SetActive(false);
+            thankYouText2.SetActive(true);
+            StartCoroutine(coroutineA());
+        }
+
+
+    }
+
+
+}
