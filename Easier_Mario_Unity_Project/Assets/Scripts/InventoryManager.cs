a60a463933414415a42c15c0c9277ccd587130a1
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2022-01-30 19:11:30 +0000

    UI For resource count

--- Easier_Mario_Unity_Project/Assets/Scripts/InventoryManager.cs
+++ Easier_Mario_Unity_Project/Assets/Scripts/InventoryManager.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InventoryManager : MonoBehaviour
 {
     public List<ResourceData> Resources;
+
+    public TMP_Text Resource;
+
+    private void Update()
+    {
+        for(int i = 0; i < Resources.Count; i++)
+        {
+            Resource.text = Resources[i].CurrentResourceValue + "/" + Resources[i].MaxResourceValue;
+        }
+    }
 }
